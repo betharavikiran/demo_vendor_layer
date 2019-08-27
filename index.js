e584a82cc919d1a0aeb9c177c015b059ab82d222
betharavikiran <betha.ravikiran@gmail.com>
2019-08-27 15:40:12 +0530

    initial draft

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+import loginCall from './src/methods/loginCall';
+
+
 function getVendorInterface(env, loginId, password){
+    loginCall(env, loginId,password);
     return "vendor interface" + env+ loginId + password;
 }
 
---
+++ src/methods/loginCall.js
@@ -0,0 +1,8 @@
+function loginCall(config, loginID, password)  {
+    console.log("in Login Call");
+    console.log(config);
+    console.log(loginID);
+    console.log(password);
+}
+
+export default loginCall;
